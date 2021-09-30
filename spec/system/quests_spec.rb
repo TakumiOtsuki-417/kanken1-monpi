8f358f6ff0330a896627d6930b93dc4c8bf0974e
TakumiOtsuki-417 <heitaku443@gmail.com>
2021-09-30 18:29:25 +0900

    結合テストコード修正

--- spec/system/quests_spec.rb
+++ spec/system/quests_spec.rb
@@ -90,7 +90,7 @@ end
       fill_in 'user_email', with: @user.email
       fill_in 'user_password', with: @user.password
       find('input[name="commit').click
-      visit new_quest_path
+      visit new_makequest_path
       expect(current_path).to eq(root_path)
     end
   end
