81ae5e9cbe958c98cbef37315ac6a5f6bb51b22a
TakumiOtsuki-417 <heitaku443@gmail.com>
2021-10-14 15:58:57 +0900

    README編集

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
          attr_accessor :code1, :code2, :code3, :code4
